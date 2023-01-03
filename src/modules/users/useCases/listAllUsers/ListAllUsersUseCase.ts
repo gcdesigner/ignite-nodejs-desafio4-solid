9bcdff5af8fea167ee9f2b212ed24acb47e31e17
gcdesigner <gcdesigner.contato@gmail.com>
2023-01-03 14:03:16 -0300

    first commit

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,21 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  // eslint-disable-next-line prettier/prettier
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not found!");
+    }
+
+    if (!user.admin) {
+      throw new Error("User not has permission to list users");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
